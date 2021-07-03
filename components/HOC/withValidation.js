541a4f9e9a73732f12cde105bb9e343353d7d4f9
rebirthtobi <ttobi4@gmail.com>
2021-07-03 18:45:17 +0100

    fixing withValidation test

--- components/HOC/withValidation.js
+++ components/HOC/withValidation.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
-const withValidation = (Wrapped, validators) => Wrapped;
+const withValidation = (Wrapped, validators) => {
+    return class extends React.PureComponent {
+        constructor(props) {
+            super(props);
+            this.state = { errors: [] };
+        }
+
+        validateOnChange = (value) => {
+            const errors = [];
+            validators.map(validator => {
+                const validationResponse = validator(value)
+                if (!validationResponse.isValid) {
+                    errors.push(validationResponse.message);
+                }
+            });
+            this.setState({ errors: [...errors] });
+            this.props.onChanged(value, !errors.length);
+        }
+
+        render() {
+            return <Wrapped validateOnChange={this.validateOnChange} errors={this.state.errors}/>;
+        }
+    }
+};
 
 export default withValidation;
