a917198b484f217c68f86b79f9df3d0374420818
rebirthtobi <ttobi4@gmail.com>
2021-07-03 17:56:14 +0100

    fixing candidate profile screen test

--- components/CandidateProfileSection.js
+++ components/CandidateProfileSection.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
-const CandidateProfileSection = () => <View />;
+const CandidateProfileSection = ({ title, children }) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+            {children}
+        </View>
+    )
+};
 
 export default CandidateProfileSection;
+
+const styles = StyleSheet.create({
+    container: {},
+    titleContainer: { alignItems: "center" },
+    title: {
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+});
+
+
--- components/CandidateProfileSectionEntry.js
+++ components/CandidateProfileSectionEntry.js
@@ -1,6 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
-const CandidateProfileSectionEntry = () => <View />;
+const CandidateProfileSectionEntry = ({ title, info }) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{`${title}:`}</Text>
+            <Text style={styles.info}>{info}</Text>
+        </View>
+    )
+};
 
 export default CandidateProfileSectionEntry;
+
+const styles = StyleSheet.create({
+    container: { padding: 8 },
+    title: {
+        fontSize: 12,
+        fontWeight: "bold"
+    },
+    info: { fontSize: 18 },
+});
+
