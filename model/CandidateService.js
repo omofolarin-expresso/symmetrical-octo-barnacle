285a205009590536175a0a4e4b08b312637ffa59
klazbaba <giwaklaz@gmail.com>
2021-09-13 17:26:49 +0100

    adding candidate test now passes

--- model/CandidateService.js
+++ model/CandidateService.js
@@ -7,8 +7,8 @@ class CandidateService {
   addCandidate = async ({
     name, surname, email, city, country, avatarUrl
   }) => {
-    this.candidates.push({ name, surname, email, city, country, avatarUrl })
-    return ({name, surname, email, city, country, avatarUrl})
+    this.candidates.push({ name, surname, email, city, country, avatarUrl, id: String(this.candidates.length + 1) })
+    return ({name, surname, email, city, country, avatarUrl, id: String(this.candidates.length)})
   };
 
   updateCandidate = async ({
@@ -17,7 +17,17 @@ class CandidateService {
 
   removeCandidate = async (id) => {};
 
-  fetchCandidates = async () => [];
+  fetchCandidates = async () => {
+    const candidatesToReturn = []
+    console.log('candidates: ', this.candidates)
+    this.candidates.forEach(candidate => candidatesToReturn.push({
+      name: candidate.name,
+      surname: candidate.surname,
+      id: candidate.id,
+      avatarUrl: candidate.avatarUrl
+    }))
+    return candidatesToReturn
+  };
 
   fetchDetails = async id => ({});
 }
