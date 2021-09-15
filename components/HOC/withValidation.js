08191c8c508f499bb3642a29fae4999a9b86bd63
Peter Okafor <peter.obinna.okafor@gmail.com>
2021-09-15 16:56:15 +0100

    feat: complete withValidation HOC

--- components/HOC/withValidation.js
+++ components/HOC/withValidation.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const withValidation = (Wrapped, validators) => Wrapped;
+// Finish the withValidation HOC located in components/withValidation.js. 
+// This component should take a Wrapped component and an array of validators and 
+// create a component with validateOnChange function passed down as a prop. 
+// This function should validate the received values using all of the validators (check model/Validators.js as an example) and 
+// pass down a list of encountered errors to the Wrapped component. 
+// The new component should also accept the onChanged prop that would be used to notify the parent about the changed value along with its current validation status.
+
+const withValidation = (Wrapped, validators) => {
+  const t = ({props, onChanged}) => {
+    const err = []
+    const validateOnChange = val => {
+      var status = true
+      err.length = 0
+      validators.map(v => {
+        const e = v(val)
+        if (!e.isValid) {
+          err.push(e.message)
+          status = e.isValid
+        }
+      })
+      onChanged(val, status)
+    }
+    return (
+      <Wrapped
+        errors={err}
+        validateOnChange={(v) => validateOnChange(v)}
+        onChanged={onChanged}
+        {...props}
+      />
+    )
+  }
+  return t
+};
 
 export default withValidation;
