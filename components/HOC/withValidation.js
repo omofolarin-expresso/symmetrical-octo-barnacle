860eee4e45e75fbc33226fc317b5fa6ab558c46c
Peter Okafor <peter.obinna.okafor@gmail.com>
2021-09-15 16:57:29 +0100

    code: remove used state and irrelevant comment

--- components/HOC/withValidation.js
+++ components/HOC/withValidation.js
@@ -1,11 +1,4 @@
-import React, { useState } from 'react';
-
-// Finish the withValidation HOC located in components/withValidation.js. 
-// This component should take a Wrapped component and an array of validators and 
-// create a component with validateOnChange function passed down as a prop. 
-// This function should validate the received values using all of the validators (check model/Validators.js as an example) and 
-// pass down a list of encountered errors to the Wrapped component. 
-// The new component should also accept the onChanged prop that would be used to notify the parent about the changed value along with its current validation status.
+import React from 'react';
 
 const withValidation = (Wrapped, validators) => {
   const t = ({props, onChanged}) => {
