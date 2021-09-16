ed58f5c95d8cc4004499ad3061b30c634bf09439
Peter Okafor <peter.obinna.okafor@gmail.com>
2021-09-16 01:07:31 +0100

    code: complete withFetching HOC

--- components/HOC/withFetching.js
+++ components/HOC/withFetching.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const withFetching = (Wrapped, Loading, loader) => Wrapped;
+const withFetching = (Wrapped, Loading, loader) => {
+  return class extends Component {
+    state = {showLoading: true, data: null}
+  
+    loadData = async () => {
+        const data = await loader();
+        this.setState({ showLoading: false, data });
+    }
+  
+    reload = () => this.setState({ showLoading: true }, () => this.loadData())
+    
+    componentDidMount = async () => (await this.loadData())
+  
+    render() {
+        const {showLoading, data} = this.state
+        return showLoading ? (
+          <Loading {...this.props.loadingProps} />
+        ) : (
+          <Wrapped {...this.props} payload={data} reload={this.reload} />
+        )
+    }
+  }
+};
 
 export default withFetching;
